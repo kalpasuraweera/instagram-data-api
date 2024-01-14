aa0ff4fb5b13ac3e2edddeaa8b986cec56b65109
kalpasuraweera <kalpamadhushan18@gmail.com>
2024-01-14 09:31:59 +0530

    init

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const app = express();
+const PORT = 3000;
+
+app.get("/:username", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://www.instagram.com/${req.params.username}/?__a=1`
+    );
+    const $ = cheerio.load(response.data);
+    const metaContent = $('meta[property="og:description"]').attr("content");
+
+    const userData = metaContent
+      .replace(/Followers|Following|Posts/g, "")
+      .split("-")[0]
+      .split(",");
+
+    function convertToNumber(arr) {
+      return arr.map((item) => {
+        const num = parseFloat(item);
+        let value = item;
+        if (item.includes("M")) {
+          value = num * 1e6;
+        } else if (item.includes("K")) {
+          value = num * 1e3;
+        } else if (item.includes("B")) {
+          value = num * 1e9;
+        } else {
+          value = num;
+        }
+        return { number: value, text: item.trim() };
+      });
+    }
+
+    // Convert and assign to variables
+    const [followersData, followingData, postsData] = convertToNumber(userData);
+
+    // Send the extracted data as JSON
+    res.json({
+      username: req.params.username,
+      followers: followersData.number,
+      followers_text: followersData.text,
+      following: followingData.number,
+      following_text: followingData.text,
+      posts: postsData.number,
+      posts_text: postsData.text,
+    });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while fetching the data." });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
