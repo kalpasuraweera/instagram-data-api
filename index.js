4cf0d1233df55500455546aaf05ea2516821213b
kalpasuraweera <kalpamadhushan18@gmail.com>
2024-02-18 00:08:01 +0530

    Add console.log to log Instagram API response data

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ app.get("/instagram/:username", async (req, res) => {
     const response = await axios.get(
       `https://www.instagram.com/${req.params.username}/?__a=1`
     );
+    console.log(response.data);
     const $ = cheerio.load(response.data);
     const metaContent = $('meta[property="og:description"]').attr("content");
 
